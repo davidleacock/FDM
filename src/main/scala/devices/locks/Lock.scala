4927cd6c8bbc2f5fbb1c6d15a938153d7b039328
davidleacock <dave.leacock@gmail.com>
2023-10-13 17:09:09 -0700

    add new device

---
+++ src/main/scala/devices/locks/Lock.scala
@@ -0,0 +1,22 @@
+package devices.locks
+
+import monocle.{Getter, Lens}
+import monocle.macros.GenLens
+
+import java.util.UUID
+
+case class Lock(id: UUID, lockStatus: LockStatus)
+
+sealed trait LockStatus
+case object Locked extends LockStatus
+case object Unlocked extends LockStatus
+
+trait LockService[F[_]] {
+  def read(lock: Lock): F[LockStatus]
+  def set(lock: Lock, lockStatus: LockStatus): F[Lock]
+}
+
+object Lock {
+  val lockStatusLens: Lens[Lock, LockStatus] = GenLens[Lock](_.lockStatus)
+  val idGetter: Getter[Lock, UUID] = Getter[Lock, UUID](_.id)
+}
