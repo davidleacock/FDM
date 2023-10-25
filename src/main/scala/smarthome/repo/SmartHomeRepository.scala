f9ffd9d4c8a37a9afb2536769f961ecd4326c1b1
davidleacock <dave.leacock@gmail.com>
2023-10-24 17:08:40 -0700

    clean and redesign api for SmartHomeRepository

--- src/main/scala/smarthome/repo/SmartHomeRepository.scala
+++ src/main/scala/smarthome/repo/SmartHomeRepository.scala
@@ -1,29 +1,20 @@
 package smarthome.repo
 
 import smarthome.SmartHome
-import smarthome.SmartHome.ContactInfo
-import smarthome.devices.Device
-import smarthome.devices.light.LightSwitch
-import smarthome.devices.motion.MotionDetector
-import smarthome.devices.thermo.Thermostat
 import smarthome.repo.SmartHomeRepository.RepositoryError
 
 import java.util.UUID
 
 trait SmartHomeRepository[F[_]] {
-
-  // TODO review this to make sure it makes sense still, these calls seem too specific
-
-  def create(ownerInfo: ContactInfo): F[Either[RepositoryError, SmartHome]]
-
-  def addThermostat(homeId: UUID, thermostat: Thermostat): F[Either[RepositoryError, SmartHome]]
-  def addLight(homeId: UUID, lightSwitch: LightSwitch): F[Either[RepositoryError, SmartHome]]
-  def addMotionDetector(homeId: UUID, motionDetector: MotionDetector): F[Either[RepositoryError, SmartHome]]
-
-  def getHome(homeId: UUID): F[Either[RepositoryError, SmartHome]]
-  def updateSmartHome[A <: Device[A]](device: A, smartHome: SmartHome): F[Either[RepositoryError, SmartHome]]
+  def create(smartHome: SmartHome): F[Either[RepositoryError, SmartHome]]
+  def update(smartHome: SmartHome): F[Either[RepositoryError, SmartHome]]
+  def retrieve(homeId: UUID): F[Either[RepositoryError, SmartHome]]
 }
 
 object SmartHomeRepository {
-  case class RepositoryError(msg: String)
+  sealed trait RepositoryError
+
+  object RepositoryError {
+    case object SmartHomeNotFound extends RepositoryError
+  }
 }
